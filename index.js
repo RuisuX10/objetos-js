70deb9a2f371b9f96df90ebfe5d872e08b6bf2c1
RuisuX10 <r.luisescobar@hotmail.com>
2023-05-30 15:03:14 -0300

    creacion de Daryl, zombie y boss zombie

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello")
+const Daryl = {
+    healt: 400,
+    damage: 20,
+
+    stats: function(){
+        console.log("-Healt = " + this.healt);
+    },
+
+    hit: function(target){
+        return target.healt -= this.damage
+    }
+}
+
+const Zombie = {
+    healt: 180,
+    damage: 18,
+
+    stats: function(){
+        console.log("-Healt = " + this.healt);
+    },
+
+    hit: function(target){
+        return target.healt -= this.damage
+    }
+}
+
+const Bosszombie = {
+    healt: 800,
+    damage: 50,
+
+    stats: function(){
+        console.log("-Healt = " + this.healt);
+    },
+
+    hit: function(target){
+        return target.healt -= this.damage
+    }
+}
+
+Daryl.stats();
+Zombie.hit(Daryl);
+Daryl.stats();
